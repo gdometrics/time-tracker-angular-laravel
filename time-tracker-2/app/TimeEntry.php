451d1f600ce773ec169b1dd6669a926e55ff13a9
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-10-25 13:51:18 -0700

    configure local sqlite database

---
+++ time-tracker-2/app/TimeEntry.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TimeEntry extends Model
+{
+    // Use the time_entries table
+    protected $table = 'time_entries';
+}
--- time-tracker-2/database/migrations/2016_10_25_202128_create_time_entries_table.php
+++ time-tracker-2/database/migrations/2016_10_25_202128_create_time_entries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTimeEntriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('time_entries', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->integer('user_id')->unsigned();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->string('comment')->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('time_entries');
     }
 }
